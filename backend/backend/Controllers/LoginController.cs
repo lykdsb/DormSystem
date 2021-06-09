9b86aa38c8d27e40c0527c189e29e3a066f95b96
来一口 <1697353364@qq.com>
2021-06-09 15:50:16 +0800

    完成登陆验证功能

---
+++ backend/backend/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SqlSugar;
+using Microsoft.AspNetCore.Mvc;
+using backend.Configs;
+using backend.Models;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json;
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class LoginController:ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Submit([FromBody]User userInput)
+        {
+            var userID = userInput.UserID;
+            var password = userInput.Password;
+            var user = DBContext.DBstatic.Queryable<User>().Single(c=>c.UserID==userID);
+            if (user == null || !password.Equals(user.Password))
+            {
+                return Ok("Wrong userID or password");
+            }
+            else
+            {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Nbf,$"{ new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
+                    new Claim(JwtRegisteredClaimNames.Exp,$"{ new DateTimeOffset(DateTime.Now.AddMinutes(30)).ToUnixTimeSeconds() }"),
+                    new Claim(ClaimTypes.NameIdentifier, Convert.ToString(userID))//自定义的 payload 部分，包含了用户的 ID 用于识别身份
+                };
+                //生成密钥用于签名
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GlobalVar.Secret));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                //生成 token
+                var token = new JwtSecurityToken(
+                    issuer: GlobalVar.Domain,
+                    audience: GlobalVar.Domain,
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+                //返回 token 给客户端使用
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token)
+                ) ;
+            }
+        }
+    }
+}
--- backend/backend/Models/User.cs
+++ backend/backend/Models/User.cs
@@ -6,7 +6,7 @@ namespace backend.Models
     {
         public int UserID { get; set; }
         public string UserName { get; set; }
-        public string password { get; set; }
-        public int access { get; set; }
+        public string Password { get; set; }
+        public int Access { get; set; }
     }
 }
